a3d53ebf52c25f0c7f7f6734cb7fd35e813102bb
Arka <arka.spectacular@gmail.com>
2021-09-03 20:34:19 +0530

    [Arka]Add:Coloumns Added

--- ADO_AddressBookService.sql
+++ ADO_AddressBookService.sql
@@ -1,2 +1,14 @@
 --UC1 Create Database
 Create Database AddressBookService
+
+--UC2 Create Table
+Create TABLE Address_Book(
+FirstName varchar(100),
+SecondName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+Zip BigInt,
+PhoneNumber BigInt,
+EmailAddress varchar(200))
+
